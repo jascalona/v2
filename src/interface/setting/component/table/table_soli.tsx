83d685d708d3ffbfcd05c69eff988584c18fc05e
jascalona <abrahamjose.1587@gmail.com>
2026-01-05 11:47:58 -0400

    se realizo ajuste de distribucion

--- src/interface/setting/component/table/table_soli.tsx
+++ src/interface/setting/component/table/table_soli.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom'; // <-- Hook para la navegación
-import '../../../../assets/css/table_task.css' 
+import '../../../../assets/css/table_task.css'
 
 //Icons
 import FlagIcon from '@mui/icons-material/Flag';
@@ -13,26 +13,26 @@ interface Solicitud {
     co_solicitud: string,
     fe_registro: string,
     fe_vencimiento: string,
-    fe_resolucion: string, 
+    fe_resolucion: string,
     fe_cierre: string,
     co_user_credor_soli: string,
     co_user_resolutor: string,
     co_tip_solicitud: string,
     nb_contacto: string,
     nu_celular_contacto: string,
-    tx_asunto: string, 
+    tx_asunto: string,
     tx_descripcion: string,
-    tx_causa: string, 
+    tx_causa: string,
     co_ambiente: string,
     co_producto: string,
-    co_sla: string, 
+    co_sla: string,
     co_user_cierre: string,
     tx_desc_resolucion: string,
-    tx_nota: string, 
-    nb_prioridad: string, 
+    tx_nota: string,
+    nb_prioridad: string,
     co_estado: string,
     co_cliente: string,
-    co_tp_solicitud: string, 
+    co_tp_solicitud: string,
 }
 
 // Para la agrupación por estado
@@ -54,8 +54,8 @@ const CalendarIcon: React.FC = () => (
 );
 
 const PriorityIcon: React.FC<{ priority: Solicitud['nb_prioridad'] }> = ({ priority }) => {
-    const priorityString = String(priority || '').trim(); 
-    let icon = <FlagIcon sx={{ fontSize: 18 }} />; 
+    const priorityString = String(priority || '').trim();
+    let icon = <FlagIcon sx={{ fontSize: 14 }} />;
     let iconClass = 'priority-icon';
 
     return (
@@ -65,10 +65,6 @@ const PriorityIcon: React.FC<{ priority: Solicitud['nb_prioridad'] }> = ({ prior
     );
 };
 
-const AddIcon: React.FC = () => (
-    <span className="add-icon">+</span>
-);
-
 
 
 // --- COMPONENTE PRINCIPAL ---
@@ -76,8 +72,8 @@ const AddIcon: React.FC = () => (
 const TaskListSoli: React.FC = () => {
     const [solicitudes, setSolicitudes] = useState<Solicitud[]>([]);
     const [cargando, setCargando] = useState(true);
-    
-    const navigate = useNavigate(); 
+
+    const navigate = useNavigate();
 
     // Función para manejar el click en la fila y la redirección
     const handleRowClick = (solicitud: Solicitud) => {
@@ -117,7 +113,7 @@ const TaskListSoli: React.FC = () => {
         }, {} as Record<string, TaskGroup>);
 
         return Object.values(groupsMap);
-    }, [solicitudes]); 
+    }, [solicitudes]);
 
 
     if (cargando) return <p>Cargando registros...</p>
@@ -126,7 +122,7 @@ const TaskListSoli: React.FC = () => {
 
     // Función auxiliar para obtener las iniciales del asignado
     const getInitials = (fullName: string | undefined): string => {
-        if (!fullName) return 'NA'; 
+        if (!fullName) return 'NA';
         const parts = fullName.split(' ').filter(p => p.length > 0);
         if (parts.length === 0) return 'NA';
         if (parts.length === 1) return parts[0].substring(0, 2).toUpperCase();
@@ -136,76 +132,88 @@ const TaskListSoli: React.FC = () => {
 
     // Renderizado de los datos agrupados
     return (
-        <div className="task-list-container">
 
-            {/* Encabezado global de la tabla */}
-            <div className="header-row">
-                <div>Asunto</div>
-                <div>Persona asignada</div>
-                <div>Fecha límite</div>
-                <div>Prioridad</div>
+        <>
+            <div className="header-left" >
+                <h2>Gestion de Clientes <span className="badge-count">{"10"} clientes</span></h2>
+                <p className="subtitle">Mantén el seguimiento de tus clientes y sus productos.</p>
             </div>
 
-            <div className="add-task-button">
-                <TaskAltIcon />
-                <span className="add-task-text">Solicitudes</span>
+            <div className="task-list-container">
+
+                {/* Encabezado global de la tabla */}
+                <div className="header-row">
+                    <div>Asunto</div>
+                    <div>Persona asignada</div>
+                    <div>Fecha límite</div>
+                    <div>Prioridad</div>
+                </div>
+
+                <div className="add-task-button">
+                    <TaskAltIcon sx={{ fontSize: 18 }} />
+                    <span className="add-task-text">Solicitudes</span>
+                </div>
+
+                {/* Renderizado de los grupos de solicitudes */}
+                {groupedTasks.map((group) => (
+                    <React.Fragment key={group.name}>
+
+                        {/* Renderizado de las solicitudes dentro del grupo */}
+                        {group.tasks.map((soli) => {
+
+                            const dueDate = soli.fe_vencimiento ? new Date(soli.fe_vencimiento) : null;
+                            const today = new Date();
+                            const isOverdue = dueDate && dueDate < today;
+
+                            const rowClass = `task-row-grid`;
+                            const dateClass = `date-text ${isOverdue ? 'urgent-date' : ''}`;
+
+                            const assignedUser = soli.co_user_resolutor;
+                            const assignedInitials = getInitials(assignedUser);
+
+                            return (
+                                <>
+
+                                    <div
+                                        key={soli.co_solicitud}
+                                        className={rowClass}
+                                        onClick={() => handleRowClick(soli)} // <-- Click que redirige
+                                        style={{ cursor: 'pointer' }}    >
+
+
+
+
+                                        <div className="task-name-cell" style={{ marginLeft: 10 }}>
+                                            {soli.tx_asunto}
+                                        </div>
+
+                                        <div>
+                                            {assignedUser ? (
+                                                <AssignedPersonIcon initials={assignedInitials} />
+                                            ) : (
+                                                <span className="person-placeholder" title="No Asignado">👤</span>
+                                            )}
+                                        </div>
+
+                                        <div className={dateClass}>
+                                            {soli.fe_vencimiento ? soli.fe_vencimiento.split('T')[0] : <CalendarIcon />}
+                                        </div>
+
+                                        <div>
+                                            {soli.nb_prioridad} <PriorityIcon priority={soli.nb_prioridad} />
+
+                                        </div>
+
+                                    </div>
+
+                                </>
+
+                            );
+                        })}
+                    </React.Fragment >
+                ))}
             </div>
-
-            {/* Renderizado de los grupos de solicitudes */}
-            {groupedTasks.map((group) => (
-                <React.Fragment key={group.name}>
-
-                    {/* Renderizado de las solicitudes dentro del grupo */}
-                    {group.tasks.map((soli) => {
-
-                        const dueDate = soli.fe_vencimiento ? new Date(soli.fe_vencimiento) : null;
-                        const today = new Date();
-                        const isOverdue = dueDate && dueDate < today;
-
-                        const rowClass = `task-row-grid`;
-                        const dateClass = `date-text ${isOverdue ? 'urgent-date' : ''}`;
-
-                        const assignedUser = soli.co_user_resolutor;
-                        const assignedInitials = getInitials(assignedUser);
-
-                        return (
-                            <div 
-                                key={soli.co_solicitud} 
-                                className={rowClass}
-                                onClick={() => handleRowClick(soli)} // <-- Click que redirige
-                                style={{cursor: 'pointer' }}    >
-
-                                {/* Columna Asunto (txAsunto) */}
-                                <div className="task-name-cell">
-                                    {soli.tx_asunto}
-                                </div>
-
-                                {/* Columna Persona asignada (c_user_resolutor) */}
-                                <div>
-                                    {assignedUser ? (
-                                        <AssignedPersonIcon initials={assignedInitials} />
-                                    ) : (
-                                        <span className="person-placeholder" title="No Asignado">👤</span>
-                                    )}
-                                </div>
-
-                                {/* Columna Fecha límite (feVencimiento) */}
-                                <div className={dateClass}>
-                                    {soli.fe_vencimiento ? soli.fe_vencimiento.split('T')[0] : <CalendarIcon />}
-                                </div>
-
-                                {/* Columna Prioridad (coPrioridad) */}
-                                <div>
-                                    <PriorityIcon priority={soli.nb_prioridad} />
-                                    {soli.nb_prioridad}
-                                </div>
-
-                            </div>
-                        );
-                    })}
-                </React.Fragment>
-            ))}
-        </div>
+        </>
     );
 };
 
